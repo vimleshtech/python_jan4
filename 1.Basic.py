8910b2bda78efc625cbb44d7cb42e7260c2325f6
vimleshtech <35063663+vimleshtech@users.noreply.github.com>
2019-01-19 13:33:46 +0530

    Day -01

---
+++ 1.Basic.py
@@ -0,0 +1,25 @@
+a = 111 # declare variable and set data
+b =222
+
+
+c = a+ b # expression
+print(c)
+
+
+#my first code
+"""
+developed by: user2
+date :
+objective :
+
+"""
+eid =11
+name ="raman"
+
+print(eid)
+print('eid is =',eid)
+print('name :',name)
+
+
+
+
---
+++ inputAndOUtput.py
@@ -0,0 +1,31 @@
+'''
+sid = input('enter sid :')
+name = input('enter name :')
+hs = input('enter hs :')
+es = input('enter es :')
+cs = input('enter cs :')
+ms = input('enter ms :')
+sc = input('enter sc:')
+
+
+print('student details...')
+print('sid :',sid)
+print('name :',name)
+print('hs:',hs)
+print('es:',es)
+print('cs:',cs)
+print('ms:',ms)
+print('hs:',hs)
+
+
+total = int(hs) + int(es) + int(cs) +int(ms) +int(sc)
+print('total score : ',total)
+
+avg = total /5
+print('avg : ',avg)
+'''
+
+a= 3
+print(a**a)
+
+
